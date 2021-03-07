00236769172ab8e7feb982dc1c98648ef7f10929
suyeon <flatn426@naver.com>
2021-03-07 20:36:42 +0900

    연습용

--- codingqueen/hello2.py
+++ codingqueen/hello2.py
@@ -1,3 +1,31 @@
-from game.sound import *
-if __name__ == "__main__":
-    echo.echo_test()
+#파이썬 기초문제 arrange(1,11) mar7,2021
+#print("Marry's cosmestic")
+#print("C\\:Window")
+#print("a\nb","a\tb")
+#print("안녕하세요.\n만나서\t\t반갑습니다.")
+#print("오늘은", "일요일")
+#print("naver","kakao","sk","samsung", sep=':')
+#print("naver;kakao;sk;samsung")
+#print("first", end="");print("second")
+#print("first",end="");print("second",end="\n");print("third")
+#print(5/3)
+
+#파이썬 기초문제 arrange(11,21)
+
+# 시가총액 = 298000000000000
+# 현재가= 50000
+# PER =15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+
+# s="hello"
+# t="python"
+# print(s+"!", t)
+
+# print(2+2*3)
+
+a=128
+print(type(a))
+a="132"
+print(type(a))
